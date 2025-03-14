9b0c2c4f5cf9422ac4e0bc6fc569dada837433d6
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2025-03-14 16:54:13 +0530

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
+import Home from './home/index.jsx'
+import Dashboard from './dashboard/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      }
+    ]
+  },
+  {
+    path: '/auth/sign-in',
+    element: <SignInPage />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
